3c5da33dcf7999923c7a32d4bfcba1a4d321384f
bitcot <bitcot@bitcots-MacBook-Air.local>
2022-08-09 10:10:53 +0530

    All 09/08/22

---
+++ CollectionViewStyle/CollectionViewStyle/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewStyle
+//
+//  Created by bitcot on 02/08/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var view:UIView!
+    
+}
---
+++ CollectionViewStyle/CollectionViewStyle/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  CollectionViewStyle
+//
+//  Created by bitcot on 02/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView:UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.view.backgroundColor = UIColor.random
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        return CGSize(width: 190, height: 200)
+    }
+    
+    
+
+}
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1.0
+        )
+    }
+}
